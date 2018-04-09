8ceecc9273af01b873db2539499fae552447755f
jackcheng <767833640@qq.com>
2018-04-09 13:10:51 -0700

    complete canny.h

--- ex2-canny/canny.h
+++ ex2-canny/canny.h
@@ -9,6 +9,8 @@
 #ifndef _CANNY_
 #define _CANNY_
 
+#include <string>
+
 // images IO class
 // read the source image and output its grey image
 class imageIO {
@@ -33,8 +35,9 @@ public:
 class canny {
 private:
   unsigned char *data;   /* input image */
-  int width;
-  int height;
+  unsigned char *answer; /* output edge image */
+  int width;             /* input image's width */
+  int height;            /* input image's height */
   int *idata;            /* output for edges */
   int *magnitude;        /* edge magnitude as detected by Gaussians */
   float *xConv;          /* temporary for convolution in x direction */
@@ -42,7 +45,32 @@ private:
   float *xGradient;      /* gradients in x direction, as detected by Gaussians */
   float *yGradient;      /* gradients in x direction,a s detected by Gaussians */
 
+  // allocate buffers for related variables
+  void allocatebuffers(unsigned char *grey);
+  // release all the buffer allocated
+  void killbuffers();
+  // compute the gradients of the image
+  int computeGradients(float kernelRadius, int kernelWidth);
+  // perform hysteresis for the edge image
+  void performHysteresis(int low, int high);
+  // follow function
+  void follow(int x1, int y1, int i1, int threshold);
+  // normalization function
+  void normalizeContrast();
+  // hypotenuse function
+  float hypotenuse(float x, float y);
+  // gaussian filter
+  float gaussian(float x, float sigma);
+  // error function
+  void catchErr(std::string error);
 
+public:
+  // constructor, read the grey image and initilize all the variables
+  canny(CImg<unsigned char>& greyImg);
+  // constructor overload, canny's parameters are defined by user
+  canny(CImg<unsigned char>& greyImg, float lowThreshold, float highthreshold, float gaussiankernelradius, int gaussiankernelwidth, int contrastnormalised);
+  // get the final edge image
+  CImg<unsigned char> getEdgeImg();
 };
 
 #endif
