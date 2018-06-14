3d32285ecf2b8a709ad7850d05e9fabfe9856993
smallGum <767833640@qq.com>
2018-06-14 03:29:22 -0700

    finish image matching

--- ex6-image-stitching/stitching.h
+++ ex6-image-stitching/stitching.h
@@ -7,6 +7,7 @@
 #include <vl/generic.h>
 #include <vl/sift.h>
 #include "CImg.h"
+#include <cmath>
 
 using namespace std;
 using namespace cimg_library;
@@ -33,11 +34,21 @@ struct point {
   point(int _col, int _row): col(_col), row(_row) {}
 };
 
+// pair of two matched key points
 struct matchedPair {
   point keyPointA;
   point keyPointB;
   float minDis;
+
   matchedPair(point _pa, point _pb, float _minDis): keyPointA(_pa), keyPointB(_pb), minDis(_minDis) {}
+};
+
+// transformation vector of the two images
+struct transVec {
+  int dx, dy;
+
+  transVec(): dx(-1), dy(-1) {}
+  transVec(int _dx, int _dy): dx(_dx), dy(_dy) {}
 };
 
 // sift class
@@ -55,7 +66,46 @@ public:
   vector<keyPoint*>& getKeyPoints() { return keyPoints; }
 };
 
+// match class
+// find out the matched pairs between two images
+class match {
+private:
+	vector<keyPoint*> AKeyPoints;
+	vector<keyPoint*> BKeyPoints;
+	vector<matchedPair> matchedPairs;
+	point matchedVec;
 
+	CImg<float> srcImgA, srcImgB;
+  CImg<float> srcImgWithKpA, srcImgWithKpB;
+  CImg<float> mixImg;
+  CImg<float> fixedMatchedImg;
+
+  void matchKeyPoints();
+	void drawOriginalKeyPoints();
+	void mergeImagesWithLines();
+	void RANSAC();
+	void drawRealKeyPoints(int maxIndex);
+
+public:
+	match(vector<keyPoint*>& A, vector<keyPoint*>& B, CImg<float>& imgA, CImg<float>& imgB);
+	point getMatchedVec() { return matchedVec; }
+};
+
+// stitch class
+// stitch two images according to the transformation vector
+class stitch {
+private:
+  CImg<float> srcImgA;
+  CImg<float> srcImgB;
+  CImg<float> stitchedImg;
+  transVec matchedVec;
+
+  void stitching();
+
+public:
+  stitch(CImg<float>& imgA, CImg<float>& imgB, int sx, int sy);
+  void saveStitchedImg(char* stitchedImgAddr);
+};
 
 void sift::toGray() {
   gray = CImg<float>(src.width(), src.height(), 1, 1);
@@ -72,23 +122,20 @@ void sift::toGray() {
 sift::sift(CImg<float>& srcImg) {
   src = CImg<float>(srcImg);
   toGray();
-  int noctaves = 4, nlevels = 2, o_min = 0;
-  vl_sift_pix *ImageData = new vl_sift_pix[gray.width() * gray.height()];
+  int noctaves = 2, nlevels = 1, o_min = 0;
+  vl_sift_pix *ImageData = new vl_sift_pix[gray.height() * gray.width()];
   cimg_forXY(gray, x, y) { ImageData[y * gray.width() + x] = gray(x, y, 0, 0); }
   VlSiftFilt *SiftFilt = NULL;
   SiftFilt = vl_sift_new(gray.width(), gray.height(), noctaves, nlevels, o_min);
 
-  int KeyPoint = 0;
   int idx = 0;
   if (vl_sift_process_first_octave(SiftFilt, ImageData) != VL_ERR_EOF) {
     while (true) {
       vl_sift_detect(SiftFilt);
-      KeyPoint += SiftFilt -> nkeys;
-      cout << "number of key points: " << KeyPoint << endl;
       VlSiftKeypoint *pKeyPoint = SiftFilt -> keys;
       for (int i = 0; i < SiftFilt -> nkeys; ++i) {
         VlSiftKeypoint TemptKeyPoint = *pKeyPoint;
-        keyPoint* newKP = new keyPoint(TemptKeyPoint.x, TemptKeyPoint.y);
+        keyPoint* newKP = new keyPoint(TemptKeyPoint.y, TemptKeyPoint.x);
         pKeyPoint++;
         unsigned char yellow[] = { 255, 255, 0 };
         src.draw_circle(TemptKeyPoint.x, TemptKeyPoint.y, TemptKeyPoint.sigma / 2, yellow, 1, ~0U);
@@ -105,7 +152,6 @@ sift::sift(CImg<float>& srcImg) {
       }
 
       if (vl_sift_process_next_octave(SiftFilt) == VL_ERR_EOF) { break; }
-      KeyPoint = 0;
     }
   }
 
@@ -113,6 +159,203 @@ sift::sift(CImg<float>& srcImg) {
   delete [] ImageData;
   ImageData = NULL;
   src.display();
+}
+
+match::match(vector<keyPoint*>& A, vector<keyPoint*>& B, CImg<float>& imgA, CImg<float>& imgB) {
+	AKeyPoints = A;
+	BKeyPoints = B;
+	srcImgA = CImg<float>(imgA);
+	srcImgB = CImg<float>(imgB);
+	srcImgWithKpA = CImg<float>(imgA);
+	srcImgWithKpB = CImg<float>(imgB);
+	mixImg = CImg<float>(srcImgA._width + srcImgB._width, max(srcImgA._height, srcImgB._height), 1, 3, 0);
+	fixedMatchedImg = CImg<float>(srcImgA._width + srcImgB._width, srcImgA._height, 1, 3, 0);
+
+	matchKeyPoints();
+	drawOriginalKeyPoints();
+	mergeImagesWithLines();
+	RANSAC();
+}
+
+void match::matchKeyPoints() {
+	for (int i = 0; i < AKeyPoints.size(); ++i) {
+		keyPoint *tempDescA = AKeyPoints[i];
+		float colA = tempDescA -> col;
+    float rowA = tempDescA -> row;
+    float *kp_desc_A = tempDescA -> descrip;
+		float minSSD = 100.0;
+    int minIndex = -1;
+    int colB = -1;
+    int rowB = -1;
+
+		for (int j = 0; j < BKeyPoints.size(); ++j) {
+			keyPoint *tempDescB = BKeyPoints[j];
+			float ssd = 0;
+			for (int k = 0; k < DIM_DESCRIPTORS; ++k) {
+      	float descA = *(kp_desc_A + k);
+        float descB = *(tempDescB -> descrip + k);
+        ssd += abs(descA - descB);
+      }
+			if (ssd < minSSD) {
+      	minSSD = ssd;
+        colB = tempDescB -> col;
+        rowB = tempDescB -> row;
+      }
+		}
+
+		if (minSSD < FEAT_DESC_GAP) {
+    	point pa(tempDescA -> col, tempDescA -> row);
+      point pb(colB, rowB);
+
+      matchedPair mpair(pa, pb, minSSD);
+      matchedPairs.push_back(mpair);
+    }
+	}
+
+  cout << ">>> matchedPairs.size: " << matchedPairs.size() << endl;
+}
+
+void match::drawOriginalKeyPoints() {
+	const double yellow[] = { 255, 255, 0 };
+	for (int i = 0; i < matchedPairs.size(); ++i) {
+		srcImgWithKpA.draw_circle(matchedPairs[i].keyPointA.col, matchedPairs[i].keyPointA.row, 3, yellow, 1.0f);
+		srcImgWithKpB.draw_circle(matchedPairs[i].keyPointB.col, matchedPairs[i].keyPointB.row, 3, yellow, 1.0f);
+	}
+	srcImgWithKpA.display("srcImgAWithKeyPoints");
+	srcImgWithKpB.display("srcImgBWithKeyPoints");
+}
+
+void match::mergeImagesWithLines() {
+	cimg_forXY(mixImg, x, y) {
+  	if (x < srcImgA._width) {
+    	if (y < srcImgA._height) {
+      	mixImg(x, y, 0, 0) = srcImgWithKpA(x, y, 0, 0);
+        mixImg(x, y, 0, 1) = srcImgWithKpA(x, y, 0, 1);
+        mixImg(x, y, 0, 2) = srcImgWithKpA(x, y, 0, 2);
+      }
+      else {
+      	mixImg(x, y, 0, 0) = 0;
+        mixImg(x, y, 0, 1) = 0;
+        mixImg(x, y, 0, 2) = 0;
+      }
+    }
+  	else {
+      if (y < srcImgB._height) {
+        mixImg(x, y, 0, 0) = srcImgWithKpB(x - srcImgA._width, y, 0, 0);
+        mixImg(x, y, 0, 1) = srcImgWithKpB(x - srcImgA._width, y, 0, 1);
+        mixImg(x, y, 0, 2) = srcImgWithKpB(x - srcImgA._width, y, 0, 2);
+      }
+      else {
+        mixImg(x, y, 0, 0) = 0;
+        mixImg(x, y, 0, 1) = 0;
+      	mixImg(x, y, 0, 2) = 0;
+      }
+  	}
+  }
+  mixImg.display("mergedImg");
+
+	const double blue[] = { 0, 255, 255 };
+  for (int i = 0; i < matchedPairs.size(); i++) {
+    int xa = matchedPairs[i].keyPointA.col;
+    int ya = matchedPairs[i].keyPointA.row;
+
+    int xb = matchedPairs[i].keyPointB.col + srcImgA._width;
+    int yb = matchedPairs[i].keyPointB.row;
+
+    mixImg.draw_line(xa, ya, xb, yb, blue);
+  }
+	mixImg.display("mergedImgWithLine");
+}
+
+void match::RANSAC() {
+	int maxInliers = 0;
+  int maxIndex = -1;
+  int inliersCount;
+
+	for (int i = 0; i < matchedPairs.size(); ++i) {
+		inliersCount = 0;
+		int xa = matchedPairs[i].keyPointA.col;
+    int ya = matchedPairs[i].keyPointA.row;
+    int xb = matchedPairs[i].keyPointB.col + srcImgA._width;
+    int yb = matchedPairs[i].keyPointB.row;
+    int deltaX = xb - xa;
+    int deltaY = yb - ya;
+
+		for (int j = 0; j < matchedPairs.size(); ++j) {
+			if (j != i) {
+        int txa = matchedPairs[j].keyPointA.col;
+        int tya = matchedPairs[j].keyPointA.row;
+        int txb = matchedPairs[j].keyPointB.col + srcImgA._width;
+        int tyb = matchedPairs[j].keyPointB.row;
+        int tdeltaX = txb - txa;
+        int tdeltaY = tyb - tya;
+        int vectorGap = (tdeltaX - deltaX) * (tdeltaX - deltaX) + (tdeltaY - deltaY) * (tdeltaY - deltaY);
+
+				if (vectorGap < INLIERS_GAP) { inliersCount++; }
+			}
+		}
+
+		if (inliersCount > maxInliers) {
+			maxInliers = inliersCount;
+			maxIndex = i;
+		}
+	}
+
+	cout << "maxIndex: " << maxIndex << ", maxInliers: " << maxInliers << endl;
+	drawRealKeyPoints(maxIndex);
+}
+
+void match::drawRealKeyPoints(int maxIndex) {
+	cimg_forXY(fixedMatchedImg, x, y) {
+    if (x < srcImgA._width) {
+      if (y < srcImgA._height) {
+        fixedMatchedImg(x, y, 0, 0) = srcImgWithKpA(x, y, 0, 0);
+        fixedMatchedImg(x, y, 0, 1) = srcImgWithKpA(x, y, 0, 1);
+        fixedMatchedImg(x, y, 0, 2) = srcImgWithKpA(x, y, 0, 2);
+      }
+      else {
+        fixedMatchedImg(x, y, 0, 0) = 0;
+        fixedMatchedImg(x, y, 0, 1) = 0;
+        fixedMatchedImg(x, y, 0, 2) = 0;
+      }
+    }
+    else {
+      if (y < srcImgB._height) {
+        fixedMatchedImg(x, y, 0, 0) = srcImgWithKpB(x - srcImgA._width, y, 0, 0);
+        fixedMatchedImg(x, y, 0, 1) = srcImgWithKpB(x - srcImgA._width, y, 0, 1);
+        fixedMatchedImg(x, y, 0, 2) = srcImgWithKpB(x - srcImgA._width, y, 0, 2);
+      }
+      else {
+        fixedMatchedImg(x, y, 0, 0) = 0;
+        fixedMatchedImg(x, y, 0, 1) = 0;
+        fixedMatchedImg(x, y, 0, 2) = 0;
+      }
+    }
+  }
+
+	int mxa = matchedPairs[maxIndex].keyPointA.col;
+  int mya = matchedPairs[maxIndex].keyPointA.row;
+  int mxb = matchedPairs[maxIndex].keyPointB.col + srcImgA._width;
+  int myb = matchedPairs[maxIndex].keyPointB.row;
+  int mdeltaX = mxb - mxa;
+  int mdeltaY = myb - mya;
+	matchedVec = point(mdeltaX, mdeltaY);
+	cout << "Real match vector: (" << mdeltaX << ", " << mdeltaY << ")" << endl;
+
+	const double blue[] = { 0, 255, 255 };
+	for (int j = 0; j < matchedPairs.size(); ++j) {
+		int txa = matchedPairs[j].keyPointA.col;
+    int tya = matchedPairs[j].keyPointA.row;
+    int txb = matchedPairs[j].keyPointB.col + srcImgA._width;
+  	int tyb = matchedPairs[j].keyPointB.row;
+  	int tdeltaX = txb - txa;
+    int tdeltaY = tyb - tya;
+    int vectorGap = (tdeltaX - mdeltaX) * (tdeltaX - mdeltaX) + (tdeltaY - mdeltaY) * (tdeltaY - mdeltaY);
+
+		if (vectorGap < INLIERS_GAP) { fixedMatchedImg.draw_line(txa, tya, txb, tyb, blue); }
+	}
+
+	fixedMatchedImg.display("mergedImgWithLine_fixed");
 }
 
 #endif
--- ex6-image-stitching/test.cpp
+++ ex6-image-stitching/test.cpp
@@ -1,10 +1,13 @@
 #include "stitching.h"
 
 int main() {
-  CImg<float> src = CImg<float>("./images1/pano1_0011.bmp");
-  sift s(src);
-  vector<keyPoint*> kps = s.getKeyPoints();
-  cout << "total key points: " << kps.size() << endl;
+  CImg<float> srcA = CImg<float>("./images1/pano1_0008.bmp");
+  CImg<float> srcB = CImg<float>("./images1/pano1_0009.bmp");
+  sift s(srcA);
+  vector<keyPoint*> kpsA = s.getKeyPoints();
+  sift s2(srcB);
+  vector<keyPoint*> kpsB = s2.getKeyPoints();
+  match m(kpsA, kpsB, srcA, srcB);
 
   return 0;
 }
